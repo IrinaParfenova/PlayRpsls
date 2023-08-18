04e5c9abe8519098a03a86bd2df14f03238da025
IrinaParfenova <142429365+IrinaParfenova@users.noreply.github.com>
2023-08-18 16:21:32 +0300

    add ExceptionActionFilter

---
+++ src/Filters/ExceptionActionFilter.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using PlayRpsls.Exceptions;
+
+namespace PlayRpsls.Filters
+{
+	public class ExceptionActionFilter : ExceptionFilterAttribute
+	{
+		public override Task OnExceptionAsync(ExceptionContext context)
+		{
+			string errorMessage = context.Exception.ToString();
+			
+			if (context.Exception is RandomizerException)
+			{
+				context.HttpContext.Response.StatusCode = 503;
+				errorMessage = context.Exception.Message;
+			}
+			else
+			{
+				context.HttpContext.Response.StatusCode = 500;
+			}
+
+			context.Result = new JsonResult(errorMessage);
+			return base.OnExceptionAsync(context);
+		}
+	}
+}
